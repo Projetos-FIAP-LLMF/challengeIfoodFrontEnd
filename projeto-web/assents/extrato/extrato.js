7b356592bf958f972b67ba1f76b8f73cc34c210f
l44ver <laurentbmvasconcelos@hotmail.com>
2024-09-14 13:58:53 -0300

    remocao do extrato

--- projeto-web/assents/extrato/extrato.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.transaction-row').forEach(row => {
-    row.addEventListener('click', () => {
-        const nextElement = row.nextElementSibling;
-        if (nextElement && nextElement.classList.contains('transaction-details')) {
-            nextElement.style.display = nextElement.style.display === 'table-row' ? 'none' : 'table-row';
-            row.querySelector('.dropdown-arrow').textContent = nextElement.style.display === 'table-row' ? '▲' : '▼';
-        }
-    });
-});
